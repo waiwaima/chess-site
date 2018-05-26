f62083f47471a71e2e648e0158d87646cd3efd11
waiwaima <zhao_xianfeng@yahoo.com>
2018-05-26 05:27:01 -0400

    Define uscfId as String in the Member model

--- src/server/models/member.js
+++ src/server/models/member.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 const schema = new Schema({
-  uscfId: Number,
+  uscfId: String,
   firstName: String,
   lastName: String,
   fullName: String,
