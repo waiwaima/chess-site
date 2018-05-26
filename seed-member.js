905ae0e3923a743524963fa32975c7d06bd6d004
waiwaima <zhao_xianfeng@yahoo.com>
2018-05-26 01:47:49 -0400

    seed USCF member info

--- seed-member.js
+++ seed-member.js
@@ -15,13 +15,13 @@ if (!args[0].startsWith('mongodb://')) {
 console.log('This script populates default USCF member data.')
 const mongodb = args[0]
 mongoose.Promise = require('bluebird')
-mongoose.connect(mongodb, {useMongoClient: true})
+mongoose.connect(mongodb, { useMongoClient: true, autoIndex: false })
 const db = mongoose.connection
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const Member = require('./src/server/models/member')
 
-let filepath = path.join(__dirname, 'data', 'players.csv')
+let filepath = path.join(__dirname, 'data', 'members.csv')
 let count = 0
 let succeeded = true
 let activePlayers = []
@@ -36,21 +36,41 @@ function execute () {
       .fromFile(filepath)
       .subscribe((json) => {
         return new Promise((resolve, reject) => {
-          console.log('reading data...')
-          console.log(JSON.stringify(json))
           if (isActivePlayer(json)) {
-            activePlayers.push(json)
-            count++
+            let name = parseName(json.MEM_NAME)
+            Member.findOneAndUpdate({ uscfId: json.MEM_ID },
+              { $set: {
+                uscfId: json.MEM_ID,
+                firstName: name.firstName,
+                lastName: name.lastName,
+                fullName: json.MEM_NAME,
+                expirationDate: json.EXPIRED,
+                state: json.STATE,
+                rating: json.R_LPB_RAT
+              }},
+              { new: true, upsert: true },
+              (err, doc) => {
+                if (err) {
+                  // console.log('Failed to write this member to DB')
+                  // console.log(json.MEM_ID)
+                } else {
+                  count++
+                  console.log(count)
+                }
+                // continue next record no matter whether this record is updated or not
+                resolve({success: true})
+              })
           } else {
-            console.log('an inactive player???')
+            // console.log('This an inactive player')
+            // console.log(json.MEM_ID)
+            // continue next record
+            resolve({success: true})
           }
-          resolve({success: true})
-          // succeeded = false
-          // reject({success: false})
         })
       }, (err) => {
         callback(err)
       }, () => {
+        // complete all rows
         if (succeeded) callback()
       })
     }
@@ -73,9 +93,23 @@ function isActivePlayer (json) {
   // inactive member
   if (json.MEM_NAME === 'INACTIVE ID') return false
   // life member
-  if (json.EXPIRED === '12/31/99') return true
-  let refDate = moment('1/1/13', 'M/D/YY')
+  if (json.EXPIRED === '12/31/99') return (!!json.R_LPB_RAT)
+  let refDate = moment('1/1/16', 'M/D/YY')
   let expireDate = moment(json.EXPIRED, 'M/D/YY')
   active = (expireDate.diff(refDate, 'days', true)) > 0
   return active
+}
+
+function parseName (fullname) {
+  let names = fullname.split(',')
+  let lastName = names[0].charAt(0).toUpperCase() + names[0].slice(1).toLowerCase()
+  let firstName = ''
+  if (names.length > 1) {
+    let pieces = names[1].split(' ')
+    for (let i = 0; i < pieces.length; i++) {
+      pieces[i] = pieces[i].charAt(0).toUpperCase() + pieces[i].slice(1).toLowerCase()
+    }
+    firstName = pieces.join(' ')
+  }
+  return {firstName: firstName, lastName: lastName}
 }
--- src/server/models/member.js
+++ src/server/models/member.js
@@ -5,12 +5,14 @@ const schema = new Schema({
   uscfId: Number,
   firstName: String,
   lastName: String,
+  fullName: String,
   expirationDate: String,
   state: String,
   rating: Number,
   email: String,
   phone: Number
 })
+schema.index({ uscfId: 1, fullName: 1 })
 schema.set('toJSON', {
   virtuals: true,
   versionKey: false
